1cfaaa64406bf9a8a7b5c9ac642bd8e25197444c
Tomas Vestelind <tomas.vestelind@gmail.com>
2024-12-03 22:41:31 +0100

    Day3 part2

--- lib/day3.rb
+++ lib/day3.rb
@@ -7,4 +7,12 @@ module Day3
       .map { |a, b| a.to_i * b.to_i }
       .sum
   end
+
+  def part2(input)
+    input
+      .gsub(/\n/, "")
+      .gsub(/don't\(\).*?do\(\)/, "")
+      .gsub(/don't\(\).*$/, "")
+      .then { part1(_1) }
+  end
 end
